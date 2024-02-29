a66e44eca00dfca04ff8052ee12fed908584f501
Daniel Nikolov <nikoloda@oregonstate.edu>
2024-02-29 14:13:47 -0800

    INSERT Working

--- app.js
+++ app.js
@@ -1,44 +1,249 @@
+// App.js
+
 /*
     SETUP
 */
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 9923;                 // Set a port number at the top so it's easy to change in the future
 
-// Express
-var express = require('express');
-var app = express();
-PORT = 9923;
+// app.js
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
 // Database
-var db = require('./database/db-connector');
+var db = require('./database/db-connector')
 
-// Handlebars
-var exphbs = require('express-handlebars');
-const { query } = require('express');
-app.engine('.hbs', exphbs.engine({
-    extname: ".hbs"
-}));
-app.set('view engine', '.hbs');
+// app.js - SETUP section
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));         // this is needed to allow for the form to use the ccs style sheet/javscript
+    
 
-/*
-    ROUTES
-*/
+// app.js
+
+
 app.get('/', function(req, res)
+{
+    // Declare Query 1
+    let query1;
+
+    // If there is no query string, we just perform a basic SELECT
+    if (req.query.lname === undefined)
     {
-        let query1 = "SELECT * FROM customer;";
-        db.pool.query(query1, function(error, rows, fields){
-            res.render('index', {data: rows});
-        })
-    });
-    app.get('/customer.html', function(req, res)
+        query1 = "SELECT * FROM customer;";
+    }
+
+    // If there is a query string, we assume this is a search, and return desired results
+    else
     {
-        let query1 = "SELECT * FROM customer;";
-        db.pool.query(query1, function(error, rows, fields){
-            res.render('index', {data: rows});
+        query1 = `SELECT * FROM customer WHERE customer_last_name LIKE "${req.query.last}%"`
+    }
+
+    // Query 2 is the same in both cases
+    let query2 = "SELECT * FROM customer;";
+
+    // Run the 1st query
+    db.pool.query(query1, function(error, rows, fields){
+        
+        // Save the people
+        let people = rows;
+        
+        // Run the second query
+        db.pool.query(query2, (error, rows, fields) => {
+            
+            // Save the planets
+            let planets = rows;
+
+            // BEGINNING OF NEW CODE
+
+            // Construct an object for reference in the table
+            // Array.map is awesome for doing something with each
+            // element of an array.
+            let planetmap = {}
+            planets.map(planet => {
+                let id = parseInt(planet.customer_id, 10);
+
+                planetmap[id] = planet["customer_first_name"];
+            })
+
+            // Overwrite the homeworld ID with the name of the planet in the people object
+            people = people.map(person => {
+                return Object.assign(person, {homeworld: planetmap[person.homeworld]})
+            })
+
+            // END OF NEW CODE
+            return res.render('index', {data: people, planets: planets});
         })
-    });
+    })
+});
+
+
+app.post('/add-person-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let homeworld = parseInt(data['input-homeworld']);
+    if (isNaN(homeworld))
+    {
+        homeworld = 'NULL'
+    }
+
+    let age = parseInt(data['input-age']);
+    if (isNaN(age))
+    {
+        age = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO customer (customer_first_name, customer_last_name, customer_email, customer_phone_num, customer_date_joined) VALUES ('${data['input-fname']}', '${data['input-lname']}', ${homeworld}, ${age}, 2024-02-29)`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM customer and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/');
+        }
+    })
+});
+
+app.post('/add-person-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let homeworld = parseInt(data.homeworld);
+    if (isNaN(homeworld))
+    {
+        homeworld = 'NULL'
+    }
+
+    let age = parseInt(data.age);
+    if (isNaN(age))
+    {
+        age = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO customer (fname, lname, homeworld, age) VALUES ('${data.fname}', '${data.lname}', ${homeworld}, ${age})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on customer
+            query2 = `SELECT customer.id, customer.fname, customer.lname, customer.homeworld, customer.age, bsg_planets.name 
+FROM customer 
+LEFT JOIN bsg_planets ON customer.homeworld = bsg_planets.id;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+app.delete('/delete-person-ajax/', function(req,res,next){
+  let data = req.body;
+  let personID = parseInt(data.id);
+  let deleteBsg_Cert_People = `DELETE FROM bsg_cert_people WHERE pid = ?`;
+  let deletecustomer= `DELETE FROM customer WHERE id = ?`;
+
+
+        // Run the 1st query
+        db.pool.query(deleteBsg_Cert_People, [personID], function(error, rows, fields){
+            if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+            }
+
+            else
+            {
+                // Run the second query
+                db.pool.query(deletecustomer, [personID], function(error, rows, fields) {
+
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                })
+            }
+})});
+
+app.put('/put-person-ajax', function(req,res,next){                                   
+  let data = req.body;
+
+  let homeworld = parseInt(data.homeworld);
+  let person = parseInt(data.fullname);
+
+  queryUpdateWorld = `UPDATE customer SET homeworld = ? WHERE customer.id = ?`;
+  selectWorld = `SELECT * FROM bsg_planets WHERE id = ?`
+
+        // Run the 1st query
+        db.pool.query(queryUpdateWorld, [homeworld, person], function(error, rows, fields){
+            if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+            }
+
+            // If there was no error, we run our second query and return that data so we can use it to update the people's
+            // table on the front-end
+            else
+            {
+                // Run the second query
+                db.pool.query(selectWorld, [homeworld], function(error, rows, fields) {
+        
+                    if (error) {
+                        console.log(error);
+                        res.sendStatus(400);
+                    } else {
+                        res.send(rows);
+                    }
+                })
+            }
+})});
+
+
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
